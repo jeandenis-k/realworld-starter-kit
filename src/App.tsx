5f5f2c78f725b62c563cdeb5670e29f717137a5c
Jean-Denis Koeck <jdkoeck@gmail.com>
2022-01-05 22:05:51 +0100

    Move Homepage, add Login and Register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,62 @@
-import { ReactLocation, Router } from "react-location";
+import { Link, Outlet, ReactLocation, Router } from "react-location";
 import "./App.css";
+import { Homepage } from "./pages/Homepage";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 function App({ location }: { location: ReactLocation }) {
   return (
     <Router
       location={location}
-      routes={[{ path: "/", element: <Homepage /> }]}
-    ></Router>
-  );
-}
-
-function Homepage() {
-  return (
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <a className="navbar-brand" href="index.html">
-          conduit
-        </a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            {/* Add "active" class when you're on that page */}
-            <a className="nav-link active" href="">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              <i className="ion-compose"></i>&nbsp;New Article
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              <i className="ion-gear-a"></i>&nbsp;Settings
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              Sign in
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              Sign up
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      routes={[
+        { path: "/", element: <Homepage /> },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ]}
+    >
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            conduit
+          </a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              {/* Add "active" class when you're on that page */}
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-compose"></i>&nbsp;New Article
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-gear-a"></i>&nbsp;Settings
+              </a>
+            </li> */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Sign in
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">
+                Sign up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </Router>
   );
 }
 
